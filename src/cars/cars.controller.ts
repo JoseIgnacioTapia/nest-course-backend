1f215c7cc8b58b50c3d7e6eb3fd1f164369f53db
Jose Ignacio <josignaciot@gmail.com>
2026-01-27 20:40:27 -0300

    Project Setup, Building Block 1: Controllers, Building Block 1: Controllers

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  // ParseIntPipe es un "Pipe" que valida que el 'id' sea un número automáticamente
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
+  }
+}
